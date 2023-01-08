6424020d6d22f18146f0701f74f48d9e9a54d9e1
mborik128 <martin.borik@gmail.com>
2023-01-08 21:05:22 +0100

    Update compiler years in comments

--- src/compiler/optimizer.ts
+++ src/compiler/optimizer.ts
@@ -1,7 +1,7 @@
 /**
  * SAA1099Tracker: Compiler optimizer
  * Copyright (c) 2017-2019 Roman Borik <pmd85emu@gmail.com>
- * Copyright (c) 2022 Martin Borik <martin@borik.net>
+ * Copyright (c) 2022-2023 Martin Borik <martin@borik.net>
  *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the "Software"),
